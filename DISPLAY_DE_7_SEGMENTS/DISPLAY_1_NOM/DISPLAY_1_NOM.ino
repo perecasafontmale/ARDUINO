0456ecc1090f1140907027e561327b1c4cae7f9e
perecasafontmale <pere.casafont.2088@lacetania.cat>
2021-04-13 10:24:30 +0200

    Update DISPLAY_1_NOM.ino

--- DISPLAY_DE_7_SEGMENTS/DISPLAY_1_NOM/DISPLAY_1_NOM.ino
+++ DISPLAY_DE_7_SEGMENTS/DISPLAY_1_NOM/DISPLAY_1_NOM.ino
@@ -8,71 +8,71 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-const int A = 5;      // donar nom al pin 5 de l’Arduino
-const int B = 6;      // donar nom al pin 6 de l’Arduino
-const int C = 7;      // donar nom al pin 8 de l’Arduino
-const int D = 8;      // donar nom al pin 9 de l’Arduino
-const int E = 9;      // donar nom al pin 10 de l’Arduino
-const int F = 10;     // donar nom al pin 11 de l’Arduino
-const int G = 11;     // donar nom al pin 12 de l’Arduino
+const int ledA = 5;      // donar nom al pin 5 de l’Arduino
+const int ledB = 6;      // donar nom al pin 6 de l’Arduino
+const int ledC = 7;      // donar nom al pin 8 de l’Arduino
+const int ledD = 8;      // donar nom al pin 9 de l’Arduino
+const int ledE = 9;      // donar nom al pin 10 de l’Arduino
+const int ledF = 10;     // donar nom al pin 11 de l’Arduino
+const int ledG = 11;     // donar nom al pin 12 de l’Arduino
 unsigned long TEMPS = 500;   // es queden leds 500ms encesos
 
 //********** Setup ****************************************************************
 void setup()
 {
-   pinMode(A, OUTPUT);
-   pinMode(B, OUTPUT);
-   pinMode(C, OUTPUT);
-   pinMode(D, OUTPUT);
-   pinMode(E, OUTPUT);
-   pinMode(F, OUTPUT);
-   pinMode(G, OUTPUT);
+   pinMode(ledA, OUTPUT);
+   pinMode(ledB, OUTPUT);
+   pinMode(ledC, OUTPUT);
+   pinMode(ledD, OUTPUT);
+   pinMode(ledE, OUTPUT);
+   pinMode(ledF, OUTPUT);
+   pinMode(ledG, OUTPUT);
  
 }
 
 void loop()
 {
   // P
-  digitalWrite(A, HIGH);  //  P
-  digitalWrite(B, HIGH);  //  P
-  digitalWrite(C, LOW);   //  P
-  digitalWrite(D, LOW);   //  P
-  digitalWrite(E, HIGH);  //  P
-  digitalWrite(F, HIGH);  //  P
-  digitalWrite(G, HIGH);  //  P
+  digitalWrite(ledA, LOW);    //  P
+  digitalWrite(ledB, LOW);    //  P
+  digitalWrite(ledC, HIGH);   //  P
+  digitalWrite(ledD, HIGH);   //  P
+  digitalWrite(ledE, LOW);    //  P
+  digitalWrite(ledF, LOW);    //  P
+  digitalWrite(ledG, LOW);    //  P
   
   delay(TEMPS);             // es queden leds 500ms encesos
   
   // E
-  digitalWrite(A, HIGH);  //  E
-  digitalWrite(B, LOW);   //  E
-  digitalWrite(C, LOW);   //  E
-  digitalWrite(D, HIGH);  //  E
-  digitalWrite(E, HIGH);  //  E
-  digitalWrite(F, HIGH);  //  E
-  digitalWrite(G, HIGH);  //  E
+  digitalWrite(ledA, LOW);    //  E
+  digitalWrite(ledB, HIGH);   //  E
+  digitalWrite(ledC, HIGH);   //  E
+  digitalWrite(ledD, LOW);    //  E
+  digitalWrite(ledE, LOW);    //  E
+  digitalWrite(ledF, LOW);    //  E
+  digitalWrite(ledG, LOW);    //  E
   
   delay(TEMPS);             // es queden leds 500ms encesos
 
   // R
-  digitalWrite(A, LOW);   //  R
-  digitalWrite(B, LOW);   //  R
-  digitalWrite(C, LOW);   //  R
-  digitalWrite(D, LOW);   //  R
-  digitalWrite(E, HIGH);  //  R
-  digitalWrite(F, LOW);   //  R
-  digitalWrite(G, HIGH);  //  R
+  digitalWrite(ledA, HIGH);   //  R
+  digitalWrite(ledB, HIGH);   //  R
+  digitalWrite(ledC, HIGH);   //  R
+  digitalWrite(ledD, HIGH);   //  R
+  digitalWrite(ledE, LOW);    //  R
+  digitalWrite(ledF, HIGH);   //  R
+  digitalWrite(ledG, LOW);    //  R
     
   delay(TEMPS);             // es queden leds 500ms encesos
 
   // E
-  digitalWrite(A, HIGH);  //  E
-  digitalWrite(B, LOW);   //  E
-  digitalWrite(C, LOW);   //  E
-  digitalWrite(D, HIGH);  //  E
-  digitalWrite(E, HIGH);  //  E
-  digitalWrite(F, HIGH);  //  E
-  digitalWrite(G, HIGH);  //  E
+  digitalWrite(ledA, LOW);    //  E
+  digitalWrite(ledB, HIGH);   //  E
+  digitalWrite(ledC, HIGH);   //  E
+  digitalWrite(ledD, LOW);    //  E
+  digitalWrite(ledE, LOW);    //  E
+  digitalWrite(ledF, LOW);    //  E
+  digitalWrite(ledG, LOW);    //  E
   
   delay(TEMPS);             // es queden leds 500ms encesos
 }
